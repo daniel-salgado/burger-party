50bfa06a101c0ceee9b2cf8087c76028fd6feaf2
daniel-salgado <daniel@salgadobr.com>
2018-01-10 16:47:52 +1300

    updating

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -7,9 +7,10 @@ const navigationItem = (props) => (
         <a
             href={props.link}
             className={props.active ? classes.active : null}
-
-        >{props.children}</a>
-    </li>
+        >
+            {props.children}
+        </a>
+    </li >
 );
 
 export default navigationItem;
